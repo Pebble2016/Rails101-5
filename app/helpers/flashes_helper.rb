b5f9008bd135af54a7268fa2e3e4830e39d93225
ypj <ypj2005@126.com>
2017-04-13 22:26:48 +0800

    addd bootstrap flash function

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -6,7 +6,7 @@ module FlashesHelper
   end
 
   def user_facing_flashes
-    flash.to_hash/slice "alert", "notice", "warning"
+    flash.to_hash.slice "alert", "notice", "warning"
   end
-    
+
   end
