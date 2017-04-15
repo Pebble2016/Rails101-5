cf1da5fc669dde53d7a3ff537d8975633241edfd
ypj <ypj2005@126.com>
2017-04-15 23:04:48 +0800

    implement my posta

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
 
   namespace :account do
     resources :groups
+    resources :posts
   end
-  
+
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
